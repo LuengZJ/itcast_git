441f7600f3712767fd0f091971cc9f8f4a85df94
LuengZJ <729016052@qq.com>
2020-08-18 20:31:00 +0800

    hello

---
+++ day50_git_test/src/test/java/com/itheima/test/demo01.java
@@ -0,0 +1,12 @@
+package com.itheima.test;
+
+/**
+ * @Author Lueng zhang jin
+ * @Date 2020/8/18 20:25
+ * @Version 2019.1
+ */
+public class demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
